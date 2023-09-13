290abe82638a2f6e0228d4cb78f313f20a7ca847
YeonHoit <pdbx12@gmail.com>
2023-09-13 20:54:19 +0900

    Card 임시 Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,72 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import CardTitle from "@/components/Card/CardTitle";
+import {
+  Card,
+  CardBody,
+  CardImage,
+  CardMoreButton,
+  CardTag,
+} from "@/components/Card/Card";
+import { HiOutlinePlusSm } from "react-icons/hi";
 
 export default function Home() {
+  const tags: string[] = [
+    "Deps",
+    "Study",
+    "React",
+    "Figma",
+    "VSCode",
+    "Intellij",
+    "Next",
+  ];
+
   return (
-    <main className={styles.main}>
-      <CardTitle />
-    </main>
+    <div style={{ padding: "10px" }}>
+      <h1>메인 페이지</h1>
+      <Card>
+        <CardImage />
+        <CardBody>
+          <CardTag>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                alignContent: "center",
+                gap: "15px",
+                flex: "1 0 0",
+                flexWrap: "wrap",
+              }}
+            >
+              {tags.map((item: string, index: number) => {
+                return (
+                  <span
+                    style={{
+                      color: "#00C7AF",
+                      fontSize: "18px",
+                      fontWeight: "400",
+                    }}
+                    key={index}
+                  >
+                    #{item}
+                  </span>
+                );
+              })}
+            </div>
+            <CardMoreButton>
+              <HiOutlinePlusSm size={24} color={"white"} />
+              <span
+                style={{
+                  color: "#FFFFFF",
+                  fontSize: "16px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "normal",
+                }}
+              >
+                More
+              </span>
+            </CardMoreButton>
+          </CardTag>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
